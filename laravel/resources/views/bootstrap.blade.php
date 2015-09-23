da6632b8336e54d7e91a77d2501fd73fe9edb4e0
qwergram <npengra317@gmail.com>
2015-09-22 19:43:55 -0700

    instead of using cdn, moved all required files to public/

--- laravel/resources/views/bootstrap.blade.php
+++ laravel/resources/views/bootstrap.blade.php
@@ -2,10 +2,10 @@
 <html>
     <head>
 
-      <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-      <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+      <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+      <link href="{{asset('css/lato.css')}}" rel="stylesheet" type="text/css">
+      <script src="{{asset('js/jquery.js')}}"></script>
+      <script src="{{asset('js/bootstrap.min.js')}}"></script>
 
 
       <title>@section('title') @show</title>
