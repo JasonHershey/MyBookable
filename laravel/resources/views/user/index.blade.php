ce001b1024ea0253af3ae92a584f2a08d9856c87
qwergram <npengra317@gmail.com>
2015-09-24 19:30:53 -0700

    changed fixed route to a asset() method

--- laravel/resources/views/user/index.blade.php
+++ laravel/resources/views/user/index.blade.php
@@ -14,9 +14,9 @@
 <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
 <!-- Morris Charts JavaScript -->
-<script src="js/plugins/morris/raphael.min.js"></script>
-<script src="js/plugins/morris/morris.min.js"></script>
-<script src="js/plugins/morris/morris-data.js"></script>
+<script src="{{asset("js/plugins/morris/raphael.min.js")}}"></script>
+<script src="{{asset("js/plugins/morris/morris.min.js")}}"></script>
+<script src="{{asset("js/plugins/morris/morris-data.js")}}"></script>
 
 
 
